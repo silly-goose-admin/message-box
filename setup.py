9b45fe2d093f33b2445a9f6b7692c397684a05b3
sillygoose <167816250+silly-goose-admin@users.noreply.github.com>
2024-11-02 06:10:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='message_box',
+    name='messagebox',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'tk',
     ],
+    author='51LLY60053',
+    author_email='51lly.g0o53a@gmail.com',
+    description='A message box package',
+    url='https://github.com/your-username/your-repo-name',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
