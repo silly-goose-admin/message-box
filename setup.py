ce28f61e675983ca0ea786dd90b266277ba553d5
sillygoose <167816250+silly-goose-admin@users.noreply.github.com>
2024-11-02 06:12:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,9 @@ setup(
     author='51LLY60053',
     author_email='51lly.g0o53a@gmail.com',
     description='A message box package',
-    url='https://github.com/your-username/your-repo-name',
+    url='https://github.com/silly-goose-admin/message-box',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
